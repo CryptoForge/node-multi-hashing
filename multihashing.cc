8950f0519502025d186a2dbec4c4ed34b9ac5bd2
CryptoForge <36178381+CryptoForge@users.noreply.github.com>
2018-02-08 18:27:59 -0800

    Update multihashing.cc

--- multihashing.cc
+++ multihashing.cc
@@ -20,6 +20,7 @@ extern "C" {
     #include "lyra2.h"
     #include "lyra2re.h"
     #include "lyra2z.h"
+    #include "lyra2z330.h"
     #include "neoscrypt.h"
     #include "nist5.h"
     #include "quark.h"
@@ -51,6 +52,26 @@ extern "C" {
 
 using namespace node;
 using namespace v8;
+
+NAN_METHOD(lyra2z330) {
+
+    if (info.Length() < 2)
+        return THROW_ERROR_EXCEPTION("You must provide two arguments.");
+
+    Local<Object> target = Nan::To<Object>(info[0]).ToLocalChecked();
+
+    if(!Buffer::HasInstance(target))
+        return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
+
+    char * input = Buffer::Data(target);
+    char *output = (char*) malloc(sizeof(char) * 32);
+
+    uint32_t input_len = Buffer::Length(target);
+
+    lyra2z330_hash(input, output);
+
+    info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
+}
 
 NAN_METHOD(lyra2rev2) {
 
@@ -620,6 +641,7 @@ NAN_METHOD(yescrypt) {
 
 
 NAN_MODULE_INIT(init) {
+    Nan::Set(target, Nan::New("lyra2z330").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(lyra2z330)).ToLocalChecked());
     Nan::Set(target, Nan::New("lyra2z").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(lyra2z)).ToLocalChecked());
     Nan::Set(target, Nan::New("lyra2rev2").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(lyra2rev2)).ToLocalChecked());
     Nan::Set(target, Nan::New("quark").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(quark)).ToLocalChecked());
